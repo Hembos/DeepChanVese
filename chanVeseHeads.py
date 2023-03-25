ad2bccbd24c6ab519e16b494f4060c43c2f15f3a
Hembos <faizrahmanov.artur@mail.ru>
2023-03-25 12:33:01 +0300

    Change model

--- chanVeseHeads.py
+++ chanVeseHeads.py
@@ -3,7 +3,7 @@ import skfmm
 import torch
 import numpy as np
 
-# from chanvese import ChanVese
+import torch.nn.functional as F
 
 import matplotlib.pyplot as plt
 
@@ -33,18 +33,14 @@ class HyperParameterModel(nn.Module):
 
         self.num_params = iterations_num * 2 + 3
 
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        image_size -= 2
-        self.avg1 = nn.AvgPool2d(3, 2)
-        image_size = math.floor((image_size - 3) / 2 + 1)
-
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        image_size -= 2
-        self.avg2 = nn.AvgPool2d(3, 2)
-        image_size = math.floor((image_size - 3) / 2 + 1)
-
-        # self.flat = nn.Flatten()
-
+        self.conv1 = nn.Conv2d(1, 32, 2)
+        image_size -= 1
+        self.avg1 = nn.AvgPool2d(2, 2)
+        image_size = math.floor((image_size - 2) / 2 + 1)
+        self.conv2 = nn.Conv2d(32, 64, 2)
+        image_size -= 1
+        self.avg2 = nn.AvgPool2d(2, 2)
+        image_size = math.floor((image_size - 2) / 2 + 1)
         self.fc1 = nn.Linear(image_size * image_size * 64, 2 * self.num_params)
         self.fc2 = nn.Linear(2 * self.num_params, self.num_params)
 
@@ -52,16 +48,14 @@ class HyperParameterModel(nn.Module):
 
     def forward(self, input):
         input = torch.unsqueeze(input, 0)
-        x = self.conv1(input)
-        x = self.avg1(x)
+        x = self.avg1(F.relu(self.conv1(input)))
 
-        x = self.conv2(x)
-        x = self.avg2(x)
+        x = self.avg2(F.relu(self.conv2(x)))
 
         x = torch.flatten(x)
 
-        x = self.fc1(x)
-        x = self.fc2(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
 
         x = self.sigmoidLayer(x)
         x *= 2
@@ -72,33 +66,25 @@ class ChanVeseFeaturesModel(nn.Module):
     def __init__(self):
         super(ChanVeseFeaturesModel, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, 15, 3, 1)
+        self.conv1 = nn.Conv2d(1, 16, 2)
 
-        self.ups1 = nn.UpsamplingBilinear2d(scale_factor=3)
+        self.conv2 = nn.Conv2d(16, 32, 2, padding=1)
 
-        self.conv2 = nn.Conv2d(15, 30, 3, 1)
+        self.ups1 = nn.UpsamplingBilinear2d(scale_factor=2)
 
-        self.conv3 = nn.Conv2d(30, 45, 4, 1)
-
-        self.conv4 = nn.Conv2d(45, 60, 5, 1)
-
-        self.conv5 = nn.Conv2d(60, 64, 14, 1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
 
         self.ups2 = nn.UpsamplingBilinear2d(scale_factor=2)
-
 
     def forward(self, input):
         input = torch.unsqueeze(input, 0)
         input = torch.unsqueeze(input, 0)
-        x = self.conv1(input)
+        x = F.relu(self.conv1(input))
+        x = F.relu(self.conv2(x))
 
         x = self.ups1(x)
+        x = F.relu(self.conv3(x))
 
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        
         x = self.ups2(x)
 
         return x
